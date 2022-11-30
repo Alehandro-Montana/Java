c79905ac9ec77f0d9c9c20994f893dd8fcb7e742
Alexander <72731610+Alehandro-Montana@users.noreply.github.com>
2022-11-30 21:17:52 +0200

    Update Main.java

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         String[] array="Lore impsulm imput termo centrum coletoman mankote".split(" ");
         String symbol ="#"; //символ на который заменяем
         int p=5;
-
+//у кожному слові k-ю букву замінити заданим символом.Якщо k більше довжини слова,коригування не виконувати
         for(int i=0;i<array.length;i++){
             if(array[i].length()>=p){
                 array[i]=array[i].substring(0,p-1)+symbol+array[i].substring(p);
